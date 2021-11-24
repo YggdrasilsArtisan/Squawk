592dd1504a5f218dc7b6e3901ebb3d5608636c7f
YggdrasilsArtisan <62152298+YggdrasilsArtisan@users.noreply.github.com>
2021-11-24 13:01:35 -0500

    Added animations and created script files for owl animations and camera movement

---
+++ Squawk/Assets/Scripts/Owl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script to control the owl's animations
+public class Owl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
