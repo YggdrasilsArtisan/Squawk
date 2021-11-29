ad07b44a1445e3bd774deb72a2e0960877a33cd3
YggdrasilsArtisan <62152298+YggdrasilsArtisan@users.noreply.github.com>
2021-11-29 18:00:32 -0500

    Attached PlayerScript and Rigidbody2D to Robin

--- Squawk/Assets/Scripts/PlayerScript.cs
+++ Squawk/Assets/Scripts/PlayerScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
 
     //Variables for player controls
