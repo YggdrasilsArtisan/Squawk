71060c86dbe448adbf99956be39946ae434264ab
YggdrasilsArtisan <62152298+YggdrasilsArtisan@users.noreply.github.com>
2021-11-30 16:05:17 -0500

    Programmed attack animation state to trigger for owl based on robin's position

--- Squawk/Assets/Scripts/Owl.cs
+++ Squawk/Assets/Scripts/Owl.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 //Script to control the owl's animations
 public class Owl : MonoBehaviour
 {
+    Animator owlAnimator;
+    Rigidbody2D owlRigidbody;
+ 
+ 
     // Start is called before the first frame update
     void Start()
     {
-        
+        owlAnimator = GetComponent<Animator>();
+        owlRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 owlPosition = owlRigidbody.transform.position;
+
+        if (owlRigidbody.position.x == GameObject.Find("Robin").transform.position.x)
+        {
+            owlAnimator.SetTrigger("Attack");
+            owlPosition.y -= 0.01f;
+            owlRigidbody.transform.position = owlPosition;
+        }
     }
 }
