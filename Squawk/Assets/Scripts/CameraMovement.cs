ff37e3b1ab51dd0798dbb6a6288998f748493226
JoshuaLuscher <joshluscher@gmail.com>
2021-12-01 15:54:46 -0500

    Readded assets & changed scene programming
    Reintroduced missing assets (score counter, obstacle, player movement.) Also added new background rendering and scripts necessary for it

---
+++ Squawk/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float cameraSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(cameraSpeed * Time.deltaTime, 0, 0);
+    }
+}
---
+++ Squawk/Assets/Scripts/LoopingBackground.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingBackground : MonoBehaviour
+{
+    public float backgroundSpeed;
+    public Renderer backgroundRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        backgroundRenderer.material.mainTextureOffset += new Vector2(backgroundSpeed * Time.deltaTime, 0f);
+    }
+}
