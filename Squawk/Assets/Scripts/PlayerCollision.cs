32e8ce2d55097fccfb9fb952f8cf177447005f6b
YggdrasilsArtisan <62152298+YggdrasilsArtisan@users.noreply.github.com>
2021-12-07 12:03:59 -0500

    Programmed sound effects

--- Squawk/Assets/Scripts/PlayerCollision.cs
+++ Squawk/Assets/Scripts/PlayerCollision.cs
@@ -5,11 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour
 {
+    public AudioClip hitClip;
+    private AudioSource sound;
+    private bool playedSound = false;
+
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+    public void PlayHitSoundEffect()
+    {
+        sound.clip = hitClip;
+        sound.PlayOneShot(hitClip);
+    }
+
+    void Update()
+    {
+        if (playedSound == true)
+        {
+            sound.Play();
+        }
+        else if (playedSound == false)
+        {
+            sound.Stop();
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
-    { 
+    {
+
         if (collision.transform.tag == "Obstacle")
         {
+            PlayHitSoundEffect();
+            playedSound = true;
             SceneManager.LoadScene(3, LoadSceneMode.Single); //Loads the fourth Scene (Game Over) in Build Settings
         }
     }
+
 }
--- Squawk/Assets/Scripts/PlayerScript.cs
+++ Squawk/Assets/Scripts/PlayerScript.cs
@@ -9,12 +9,17 @@ public class PlayerScript : MonoBehaviour
     public float speed;
     private Rigidbody2D rb;
     private Vector2 playerDirection;
+    private AudioSource sound;
+    public AudioClip collectibleClip;
+    public AudioClip powerUpClip;
+    private bool playedSound = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sound = GetComponent<AudioSource>();
     }
 
 
@@ -23,13 +28,36 @@ public class PlayerScript : MonoBehaviour
     {
         float directionY = Input.GetAxisRaw("Vertical");
         playerDirection = new Vector2(0, directionY).normalized;
+
+        //Check if sound was played
+        if (playedSound == true)
+        {
+            playedSound = false;
+        }
+        else if (playedSound == false)
+        {
+            sound.Stop();
+        }
+        
     }
 
-  
+
     public void FixedUpdate()
     {
         //Controls player movement speeds
         rb.velocity = new Vector2(0, playerDirection.y * speed);
+    }
+
+    public void PlayCollectibleSoundEffect()
+    {
+        sound.clip = collectibleClip;
+        sound.PlayOneShot(collectibleClip);
+    }
+
+    public void PlayPowerUpSoundEffect()
+    {
+        sound.clip = powerUpClip;
+        sound.PlayOneShot(powerUpClip);
     }
 
 
@@ -38,27 +66,36 @@ public class PlayerScript : MonoBehaviour
     {
         if (collision.CompareTag("Seed"))
         {
+            PlayCollectibleSoundEffect();    
             Destroy(collision.gameObject);
+            playedSound = true;
 
             ScoreManager.instance.AddPoint();
         }
 
        if (collision.CompareTag("Speedberry"))
         {
-            Destroy(collision.gameObject); 
+            PlayPowerUpSoundEffect();
+            Destroy(collision.gameObject);
+            playedSound = true;
         }
 
         if (collision.CompareTag("Invinciberry"))
         {
+            PlayPowerUpSoundEffect(); 
             Destroy(collision.gameObject);
-
+            playedSound = true;
         }
 
         if (collision.CompareTag("Feather"))
         {
+            PlayCollectibleSoundEffect();
             Destroy(collision.gameObject);
+            playedSound = true;
 
             StaminaBar.instance.regenStamina();
+
         }
     }
+
 }
